6e2a007734e6d5ad30f1dcb8a9ffc07bb71f5c3a
Anton Revenok <arevenok@us.ibm.com>
2018-06-03 20:44:10 +0300

    Add right nav

--- src/js/components/pages/Header.js
+++ src/js/components/pages/Header.js
@@ -6,21 +6,17 @@ class Header extends Component {
     return(
       <header className="header">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <NavLink to='/'>Riomedia</NavLink>
+          <NavLink className="rio-logo" to='/'>Riomedia</NavLink>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"/>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <div className="nav-link">
-                  <NavLink to='/' activeClassName='menu selected' exact>Главная</NavLink>
-                </div>
+                <NavLink to='/' className="nav-link" activeClassName='menu selected' exact>Главная</NavLink>
               </li>
               <li className="nav-item">
-                <div className="nav-link">
-                  <NavLink to='/about' activeClassName='menu selected'>О нас</NavLink>
-                </div>
+                <NavLink to='/about' className="nav-link" activeClassName='menu selected'>О нас</NavLink>
               </li>
             </ul>
           </div>
