6cb68f740d30c852e1b8ea1763d6458d882b1425
Anton Revenok <arevenok@us.ibm.com>
2018-06-06 17:19:18 +0300

    Add pretty loader

--- src/js/components/helpers/MainLoader.js
+++ src/js/components/helpers/MainLoader.js
@@ -2,9 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const MainLoader = (props) => (
-  <div>
+  <div className="container">
     {
-      props.busy && <div className="loader"><div className="drip" /></div>
+      props.busy &&
+      <div className="loader">
+        <div className="page-bg"/>
+        <div className="preloader">
+          <div className="preloader-box">
+            <div>Р</div>
+            <div>И</div>
+            <div>О</div>
+            <div>М</div>
+            <div>Е</div>
+            <div>Д</div>
+            <div>И</div>
+            <div>А</div>
+          </div>
+        </div>
+
+      </div>
     }
   </div>
 );
