83a253d3c51aa14cabf7de90c0b7dd32690612e1
Anton Revenok <arevenok@us.ibm.com>
2018-06-06 18:04:51 +0300

    Add sticky footer

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={this.props.store}>
-        <MainPage />
+        <MainPage/>
       </Provider>
     )
   }
--- src/js/components/pages/HomePage.js
+++ src/js/components/pages/HomePage.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import { Jumbotron, Button, Row, Col } from 'reactstrap';
 class HomePage extends Component {
   render() {
     return (
-      <div className="rio-container">
-        <h1>Главная Страница</h1>
+      <div className="rio-content">
+        <Row>
+          <Col className="col-md-5">
+            <Jumbotron>
+              <h3>Изготовление наружной рекламы в Минске</h3>
+              <p>
+                Человек, который дорожит своим бизнесом,
+                ни за что в жизни не станет экономить на рекламе.
+                Но такое многообразие способов рекламы товаров и услуг заставляет людей долго анализировать
+                и выбирать изготовителя.
+              </p>
+              <hr className="my-2" />
+              <p>Рекламно - производственная компания «РиоМедиа»
+                специализируется на изготовлении и размещении наружной рекламы в Минске.
+                Мы имеем собственную производственную базу и производим объекты наружной рекламы из современных и долговечных материалов.</p>
+              <p className="lead">
+                <Button color="primary">Отправить заявку</Button>
+              </p>
+            </Jumbotron>
+          </Col>
+        </Row>
       </div>
     );
   }
