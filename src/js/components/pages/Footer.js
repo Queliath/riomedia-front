ab63ca9531518917499944929fbd00ab2d8bae9f
Anton Revenok <arevenok@us.ibm.com>
2018-06-07 13:55:12 +0300

    Add footer data and styles

--- src/js/components/pages/Footer.js
+++ src/js/components/pages/Footer.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Row, Col, Container } from 'reactstrap';
 
-export const Footer = () => (
-  <footer className="footer">
-    <div className="container">
-      <span className="text-muted">Riomedia footer</span>
-    </div>
-  </footer>
-);
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+        <div className="footer-container">
+          <Container>
+            <Row>
+
+              <Col lg="4" md="4" xs="12" className="footer-item">
+                <a className="topbar-item tel" href="tel:+37529673222">+375 (29) 67-32-22</a>
+                <span>Позвоните нам</span>
+              </Col>
+              <Col lg="4" md="4" xs="12" className="footer-item">
+                <a className="topbar-item" href="mailto:riomedia@ramble.ru">riomedia@ramble.ru</a>
+                <span>Отправьте нам сообщение</span>
+              </Col>
+              <Col lg="4" md="4" xs="12" className="footer-item">
+                <NavLink to="/contacts" className="topbar-item">
+                  Приходите к нам в офис
+                </NavLink>
+                <span>Получите бесплатную консультацию</span>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export { Footer };
